2f8655f03e4cfcd4277a63e5db139635af9ceafd
Sham <diego.baum33@gmail.com>
2023-01-16 19:05:27 -0300

    Seccion Completa

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
-interface Personaje{
-  nombre: string;
-  poder: number;
-}
 
 @Component({
   selector: 'app-main-page',
@@ -12,34 +10,15 @@ interface Personaje{
 })
 export class MainPageComponent {
 
-personajes: Personaje[] = [
-  {
-    nombre: 'Goku',
-    poder: 15000
-  },
-  {
-    nombre: 'Vegeta',
-    poder: 7500
-  }
-
-]
 
 
-  nuevo: Personaje = {
-    nombre: '',
-    poder: 0,
+nuevo: Personaje = {
+  nombre: '',
+  poder: 0,
+}
 
-  }
 
-  agregar(){
-    if ( this.nuevo.nombre.trim().length === 0){
-      return;
-    }
-
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre:'', poder: 0
-    }
-  }
-
+constructor( )
+{
+}
 }
